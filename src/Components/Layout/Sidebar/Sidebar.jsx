612a4a66b24aa37b057deabd27d6174d9e0be1e4
Gourav <gouravpaliwal196@gmail.com>
2024-12-25 11:32:38 +0530

    Integrate NewPost component into Sidebar for enhanced post creation functionality

--- src/Components/Layout/Sidebar/Sidebar.jsx
+++ src/Components/Layout/Sidebar/Sidebar.jsx
@@ -40,6 +40,7 @@ import { Context } from "../../../Context/Context";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { Link } from "react-router-dom";
+import Newpost from "../../Shared/NewPost/Newpost";
 
 const Sidebar = () => {
   const options = [
@@ -55,50 +56,9 @@ const Sidebar = () => {
     dark,
     setDark,
     login,
-    newPost,
-    setNewPost,
-    handleChange,
-    postTitle,
-    setPostTitle,
-    PostSubTitle,
-    setPostSubtitle,
-    postDescription,
-    setPostDescription,
-    posts,
-    setPosts
+    setDialogOpen,
   } = useContext(Context);
-  const [dialogOpen, setDialogOpen] = useState(false);
-  const [discardPost, setDiscardPost] = useState(false);
-  const [alert, setAlert] = useState(false);
-  const [isPending, setTransition] = useTransition();
 
-  const VisuallyHidenInput = styled("input")({
-    border: 0,
-    clip: "rect(0 0 0 0)",
-    height: 1,
-    margin: -1,
-    overflow: "hidden",
-    padding: 0,
-    position: "absolute",
-    whiteSpace: "nowrap",
-    width: 1,
-  });
-
-  const handlePost = async () => {
-     setTransition(() => {
-      if (newPost !== "" && postTitle !== "" && postDescription !== "" && postSubtitle !== ""){
-        console.log("Post created");
-        setPosts([...posts, {newPost, postTitle, postSubtitle, postDescription}]);
-        handleClose();
-      } else {
-        setAlert(true);
-      }
-    });
-  };
-
-  const handleClose = () => {
-    setDialogOpen(false);
-  };
 
   const handlOpen = () => {
     setDialogOpen(true);
@@ -173,179 +133,7 @@ const Sidebar = () => {
                 </Fab>
               </Tooltip>
             )}
-            <Dialog
-              fullWidth
-              maxWidth="sm"
-              open={dialogOpen}
-              component="form"
-              onClose={() => {
-                handleClose();
-              }}
-            >
-              <DialogActions sx={{ padding: "0" }}>
-                <IconButton
-                  disableRipple
-                  onClick={() => {
-                    setDiscardPost(true);
-                  }}
-                >
-                  <Close />
-                </IconButton>
-                {/* Discard post dialog */}
-                <Dialog open={discardPost} maxWidth="xl">
-                  <DialogContent>
-                    <Typography fontWeight={600} textAlign="center">
-                      Discard post ?
-                    </Typography>
-                  </DialogContent>
-                  <DialogActions>
-                    <Button
-                      color="error"
-                      fullWidth
-                      onClick={() => {
-                        setNewPost("");
-                        setDiscardPost(false);
-                        setAlert(false);
-                        handleClose();
-                      }}
-                    >
-                      Discard
-                    </Button>
-                    <Button
-                      color="success"
-                      fullWidth
-                      onClick={() => {
-                        setDiscardPost(false);
-                      }}
-                    >
-                      Cancel
-                    </Button>
-                  </DialogActions>
-                </Dialog>
-              </DialogActions>
-              <DialogTitle textAlign="center" fontWeight={600}>
-                Create post
-              </DialogTitle>
-              <DialogContent>
-                <Stack direction="row" spacing={2} alignItems="center">
-                  <Avatar src="https://lh3.googleusercontent.com/a/ACg8ocLxX3JkRQ7iWSxTVMJLFswL-GHDuf92403Q6_apGXmexnVXVZg=s360-c-no" />{" "}
-                  <Typography>Gourav Paliwal</Typography>
-                </Stack>
-                <Stack
-                  direction="row"
-                  spacing={2}
-                  alignItems="start"
-                  sx={{ marginTop: 2 }}
-                >
-                  <Box component={"div"} sx={{ width: "60%", height: "100%" }}>
-                    {newPost !== "" ? (
-                      <img src={newPost} width={"100%"} height={"100%"} />
-                    ) : (
-                      <Box
-                        component={"div"}
-                        sx={{
-                          width: "100%",
-                          height: "270px",
-                          border: "1px dashed",
-                          borderRadius: "5px",
-                          display: "flex",
-                          justifyContent: "center",
-                          alignItems: "center",
-                        }}
-                      >
-                        <Typography
-                          textAlign="center"
-                          fontWeight={600}
-                          color="text.secondary"
-                        >
-                          Upload a photo
-                        </Typography>
-                      </Box>
-                    )}
-                  </Box>
-                  <Stack
-                    direction="column"
-                    spacing={2}
-                    alignItems="start"
-                    width={"100%"}
-                  >
-                    <TextField
-                      color="secondary"
-                      id="outlined-multiline-static"
-                      multiline
-                      rows={1}
-                      label="Title"
-                      variant="outlined"
-                      fullWidth
-                      margin="normal"
-                    />
-                    <TextField
-                      color="secondary"
-                      id="outlined-multiline-static"
-                      multiline
-                      label="subtitle"
-                      rows={1}
-                      variant="outlined"
-                      fullWidth
-                      margin="normal"
-                    />
-                    <TextField
-                      color="secondary"
-                      id="outlined-multiline-static"
-                      multiline
-                      label="Description"
-                      rows={4}
-                      variant="filled"
-                      fullWidth
-                      margin="normal"
-                    />
-                  </Stack>
-                </Stack>
-              </DialogContent>
-              {alert && (
-                <Alert severity="warning" sx={{ margin: 1 }} color="warning">
-                  Please add a Photo or a Description
-                </Alert>
-              )}
-              <DialogActions sx={{ justifyContent: "start" }}>
-                <IconButton color="secondary">
-                  <EmojiEmotions />
-                </IconButton>
-                <IconButton
-                  sx={{ color: "primary.light" }}
-                  role={undefined}
-                  component="label"
-                >
-                  <Photo />
-                  <VisuallyHidenInput
-                    type="file"
-                    onChange={(event) => {
-                      setNewPost(handleChange(event));
-                    }}
-                  />
-                </IconButton>
-                <IconButton color="success">
-                  <Videocam />
-                </IconButton>
-                <IconButton sx={{ color: "error.light" }}>
-                  <PersonAdd />
-                </IconButton>
-              </DialogActions>
-              <DialogActions>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  fullWidth
-                  sx={{ marginBottom: 3 }}
-                  onClick={() => {
-                    handlePost();
-                  }}
-                  disabled={isPending}
-                >
-                  {isPending ? "Posting..." : "Post"}
-                </Button>
-              </DialogActions>
-            </Dialog>
+            <Newpost/>
           </Stack>
         </Stack>
       </Toolbar>
